4e6b2a43d41339e12388f7092e9d82b19897f497
jeremiah-ndiritu <ndiritujeremiah24@gmail.com>
2026-03-16 21:49:29 +0300

    remove css references in about and contact files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import "./AboutUs.css";
-
 function AboutUs() {
   return (
     <div className="about">
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import "./ContactUs.css";
-
 function ContactUs() {
   return (
     <div className="contact">
